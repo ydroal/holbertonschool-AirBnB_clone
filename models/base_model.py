27fc2ab52955a66b4635200b1b49cbe5b4eb51ff
bapt2 <5711@holbertonstudents.com>
2023-02-20 06:46:48 -0800

    delete a space at line 21

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
     def __str__(self):
         '''Method that return a string to print the instance.'''
 
-        return f'[{self.__class__. __name__}]({self.id}) {self.__dict__}'
+        return f'[{self.__class__.__name__}]({self.id}) {self.__dict__}'
 
     def save(self):
         '''
