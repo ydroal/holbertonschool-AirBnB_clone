e71dc4d04541e9cf39a22951720779223ef6e24e
ydroal <droal.yoko@gmail.com>
2023-02-20 06:37:17 -0800

    update: base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python3
-"""  """
+'''Module to define a BaseModel'''
+
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """  """
-    
+    '''Define a BaseModel'''
+
+    def __init__(self, id=None, created_at=None, updated_at=None, ):
+        '''Initialize'''
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        '''Method that return a string to print the instance.'''
+
+        return f'[{self.__class__. __name__}]({self.id}) {self.__dict__}'
+
+    def save(self):
+        '''
+        Method that updates the public instance attribute updated_at
+        with the current datetime
+        '''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        '''
+        Method that returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        '''
+
+        res = self.__dict__.copy()
+        res['__class__'] = __class__.__name__
+        res['created_at'] = datetime.isoformat(self.created_at)
+        res['updated_at'] = datetime.isoformat(self.updated_at)
+
+        return res
