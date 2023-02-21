f1460b6fbe80f448f0fab4e3992a32d398bc6218
bapt2 <5711@holbertonstudents.com>
2023-02-21 01:01:19 -0800

    fixed 2nd corret output

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,15 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''Initialize'''
-        if kwargs is not None:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 setattr(self, key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''Method that return a string to print the instance.'''
