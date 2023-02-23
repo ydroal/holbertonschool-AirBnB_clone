277d03d8e00d8df4978ebfa1971e34da9f92ad12
ydroal <droal.yoko@gmail.com>
2023-02-23 11:15:05 -0800

    update: file_storage.py -cp

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,54 +1,76 @@
 #!/usr/bin/python3
-'''Module to define a FileStorage'''
+"""model de storage"""
 
-import json
+
 from models.base_model import BaseModel
+import json
+import os
 from models.user import User
-from models.place import Place
-from models.state import State
 from models.city import City
+from models.state import State
+from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
-
 class FileStorage:
-    '''Define a FileStorage'''
+    """class pour file storage"""
+
+    """__file_path est un chemin vers un fichier JSON dans lequel nous enregistrerons 
+    les objets. Lorsque nous créons une instance de la classe FileStorage"""
 
     __file_path = 'file.json'
+
+    """__objects est un dictionnaire qui stocke les objets créés par l'application 
+    en utilisant leur nom de classe et leur identifiant unique.
+    BaseModel.1234 
+    En utilisant ces deux variables, la classe FileStorage est capable de stocker 
+    et de récupérer les objets créés par l'application."""
+
     __objects = {}
 
-    def all(self):
-        '''Method returns the dictionary __objects'''
+    """La méthode all() de la classe FileStorage retourne le dictionnaire 
+    des objets stockés dans __objects."""
 
+    def all(self):
+        """return dictionary of __objects"""
         return FileStorage.__objects
 
+
+    """La méthode new(self, obj) de la classe FileStorage est une méthode qui permet d'ajouter 
+    un objet au dictionnaire __objects de la classe FileStorage
+    "BaseModel.123456": {"id": "123456", "name": "example", "created_at": "2022-02-22T12:00:00", "updated_at": "2022-02-22T12:00:00"}"""
+
+    """Cette methode est appeler a la acreation de l'object dans la methode BaseMethode"""
     def new(self, obj):
-        '''Method sets in __objects the obj with key <obj class name>.id'''
+        """set in __objects with key"""
         if obj:
-            k = f'{obj.__class__.__name__}.{obj.id}'
-            FileStorage.__objects[k] = obj
+            """BaseModel.123456": {"id": "123456", "name": "example", "created_at": "2022-02-22T12:00:00", "updated_at": "2022-02-22T12:00:00"}"""
+            FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
+
+    """La méthode save() de la classe FileStorage a pour objectif de sérialiser 
+    (convertir en format JSON) le dictionnaire __objects de la classe FileStorage 
+    et d'écrire le résultat de cette sérialisation dans un fichier."""
+
+    """Cette methode est appeler par BaseModel qui elle est appeler par 
+    La console lors de la creation du modele avec la commande Create BaseModel"""
     def save(self):
-        '''Method serializes __objects to the JSON file (path: __file_path)'''
+        """serializes __objects to the JSON"""
+        jdict = {}
+        for keys, values in FileStorage.__objects.items():
+            jdict[keys] = values.to_dict()
 
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as json_file:
-            json.dump(new_dict, json_file)
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(jdict, file)
+
+    """Cette méthode permet de désérialiser le fichier JSON en objets Python 
+    et de les stocker dans le dictionnaire __objects."""
 
     def reload(self):
-        '''
-        Method that deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
-        If the file doesn't exist, no exception should be raised)
-        '''
-
-        try:
-            with open(FileStorage.__file_path) as f:
-                new_dict = json.load(f)
-                cls = '__class__'
-                for key, value in new_dict.items():
-                    FileStorage.__objects[key] = eval(value[cls] + '(**value)')
-        except FileNotFoundError:
-            pass
+        """deserializes the JSON file to __objects"""
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as file:
+                new_object_dict = json.load(file)
+            for keys, val in new_object_dict.items():
+                obj = eval(val['__class__'])(**val)
+                FileStorage.__objects[keys] = obj
