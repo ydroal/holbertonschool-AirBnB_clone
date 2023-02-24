3e3059fecf0b1a4ada0c1a97ac4e24fdd8c5a16a
bapt2 <5711@holbertonstudents.com>
2023-02-24 05:42:26 -0800

    added def

--- models/test_user.py
+++ models/test_user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ unittest for user file """
 from models.base_model import BaseModel
-from models.user import user
+from models.user import User
 import email
 import unittest
 
 
 class Test_User(unittest.TestCase):
     """ unittest for user class """
-    user = User
-    self.assertEqual("", user.email)
+    def test_user(self):
+        user = User
+        self.assertEqual("", user.email)
 
-    self.assertEqual("", user.password)
+        self.assertEqual("", user.password)
 
-    self.assertEqual("", user.first_name)
+        self.assertEqual("", user.first_name)
 
-    self.assertEqual("", user.last_name)
+        self.assertEqual("", user.last_name)
