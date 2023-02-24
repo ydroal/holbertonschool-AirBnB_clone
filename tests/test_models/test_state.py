fc2e4bebfae8f3515f4d0bcc1a7d6a0fd6f1d2e9
bapt2 <5711@holbertonstudents.com>
2023-02-24 01:01:28 -0800

    added unittest for state file

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" unittest for State """
+from models.state import State
+from models.basse_model import BaseModel
+import unittest
+
+class Test_State(unittest.TestCase):
+    """ unittest for State class """
+    def test_name(self):
+        state = State()
+        self.assertEqual("", state.name)
