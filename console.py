9792a2f165adc0e0d73629eb933eba1ccf1d7d63
bapt2 <5711@holbertonstudents.com>
2023-02-24 07:25:48 -0800

    re add EOF

--- console.py
+++ console.py
@@ -23,6 +23,10 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
 
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
     def do_quit(self, line):
         """ Quit command to exit the program """
         return True
